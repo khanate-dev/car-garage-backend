7a852b0a18e8ba77efe1d69ddd0499665a24c502
mbm_1607 <muhammadkhan1607@gmail.com>
2022-09-24 21:02:18 +0500

    Add review schema

---
+++ src/schemas/review/review.schemas.ts
@@ -0,0 +1,69 @@
+import { isValidObjectId, Types } from 'mongoose';
+import z from 'zod';
+
+import { createRouteSchema } from '~/helpers/schema';
+
+import { reviewModelSchema } from '~/models';
+
+const body = z.strictObject({
+	rating: z.number({
+		required_error: 'rating is required',
+	}).min(1).max(5),
+	description: z.string({
+		required_error: 'description is required',
+	}).optional(),
+	userId: z.instanceof(Types.ObjectId).optional(),
+	bodyTypeId: z.instanceof(Types.ObjectId).optional(),
+});
+
+const params = z.strictObject({
+	_id: z.string().refine(
+		isValidObjectId,
+		'parameter must be a valid mongo ObjectID'
+	),
+	userId: z.string().refine(
+		isValidObjectId,
+		'parameter must be a valid mongo ObjectID'
+	),
+	bodyTypeId: z.string().refine(
+		isValidObjectId,
+		'parameter must be a valid mongo ObjectID'
+	),
+});
+
+const response = reviewModelSchema;
+
+export const createReviewSchema = createRouteSchema({
+	body,
+	response,
+});
+
+export type CreateReviewSchema = z.infer<typeof createReviewSchema>;
+
+export const updateReviewSchema = createRouteSchema({
+	body,
+	params,
+	response,
+});
+
+export type UpdateReviewSchema = z.infer<typeof updateReviewSchema>;
+
+export const getReviewsSchema = createRouteSchema({
+	response: z.array(response),
+});
+
+export type GetReviewsSchema = z.infer<typeof getReviewsSchema>;
+
+export const getReviewSchema = createRouteSchema({
+	params,
+	response,
+});
+
+export type GetReviewSchema = z.infer<typeof getReviewSchema>;
+
+export const deleteReviewSchema = createRouteSchema({
+	params,
+	response,
+});
+
+export type DeleteReviewSchema = z.infer<typeof deleteReviewSchema>;
