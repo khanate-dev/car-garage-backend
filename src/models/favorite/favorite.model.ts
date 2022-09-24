f0c97a527ab8c58ade05cb98572885409d4ed889
mbm_1607 <muhammadkhan1607@gmail.com>
2022-09-24 20:21:44 +0500

    Add favorite model

---
+++ src/models/favorite/favorite.model.ts
@@ -0,0 +1,36 @@
+import { Schema, model, Types } from 'mongoose';
+import z from 'zod';
+
+import { getModelSchema } from '~/helpers/schema';
+
+export const {
+	sansMetaModelSchema: favoriteSansMetaModelSchema,
+	modelSchema: favoriteModelSchema,
+} = getModelSchema({
+	userId: z.instanceof(Types.ObjectId),
+	bodyTypeId: z.instanceof(Types.ObjectId),
+});
+
+export type FavoriteSansMeta = z.infer<typeof favoriteSansMetaModelSchema>;
+
+export type Favorite = z.infer<typeof favoriteModelSchema>;
+
+const favoriteSchema = new Schema<Favorite>(
+	{
+		userId: {
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		bodyTypeId: {
+			type: Schema.Types.ObjectId,
+			ref: 'BodyType',
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+export const FavoriteModel = model('Favorite', favoriteSchema);
