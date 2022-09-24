602c4ba31020e1e33e6e4960d39111cfba514dba
mbm_1607 <muhammadkhan1607@gmail.com>
2022-09-25 00:19:16 +0500

    Add review CRUD routes

---
+++ src/routes/review/review.routes.ts
@@ -0,0 +1,58 @@
+import {
+	createReviewSchema,
+	deleteReviewSchema,
+	getReviewSchema,
+	updateReviewSchema,
+	getReviewsSchema,
+} from '~/schemas/review';
+
+
+import {
+	createReviewHandler,
+	deleteReviewHandler,
+	getReviewHandler,
+	updateReviewHandler,
+	getReviewsHandler,
+} from '~/controllers/review';
+
+import { Route } from '~/types';
+
+const reviewRoutes: Route[] = [
+	{
+		method: 'post',
+		path: '/',
+		schema: createReviewSchema,
+		handler: createReviewHandler,
+		isAuthenticated: true,
+	},
+	{
+		method: 'get',
+		path: '/',
+		schema: getReviewsSchema,
+		handler: getReviewsHandler,
+		isAuthenticated: true,
+	},
+	{
+		method: 'get',
+		path: '/:_id',
+		schema: getReviewSchema,
+		handler: getReviewHandler,
+		isAuthenticated: true,
+	},
+	{
+		method: 'put',
+		path: '/:_id',
+		schema: updateReviewSchema,
+		handler: updateReviewHandler,
+		isAuthenticated: true,
+	},
+	{
+		method: 'delete',
+		path: '/:_id',
+		schema: deleteReviewSchema,
+		handler: deleteReviewHandler,
+		isAuthenticated: true,
+	},
+];
+
+export default reviewRoutes;
