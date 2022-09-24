af4daf9c462ff6cf8dba0d1754cb896980311c87
mbm_1607 <muhammadkhan1607@gmail.com>
2022-09-24 20:16:15 +0500

    Add review model

---
+++ src/models/review/review.model.ts
@@ -0,0 +1,48 @@
+import { Schema, model, Types } from 'mongoose';
+import z from 'zod';
+
+import { getModelSchema } from '~/helpers/schema';
+
+export const {
+	sansMetaModelSchema: reviewSansMetaModelSchema,
+	modelSchema: reviewModelSchema,
+} = getModelSchema({
+	rating: z.number().min(1).max(5),
+	description: z.string().optional(),
+	userId: z.instanceof(Types.ObjectId),
+	bodyTypeId: z.instanceof(Types.ObjectId),
+});
+
+export type ReviewSansMeta = z.infer<typeof reviewSansMetaModelSchema>;
+
+export type Review = z.infer<typeof reviewModelSchema>;
+
+const reviewSchema = new Schema<Review>(
+	{
+		rating: {
+			type: Number,
+			required: true,
+			min: 1,
+			max: 5,
+		},
+		description: {
+			type: String,
+			required: false,
+		},
+		userId: {
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		bodyTypeId: {
+			type: Schema.Types.ObjectId,
+			ref: 'BodyType',
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+export const ReviewModel = model('Review', reviewSchema);
