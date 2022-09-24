c4e0c43ba2860cff9a0aa880b80ac5fa0058b23d
mbm_1607 <muhammadkhan1607@gmail.com>
2022-09-24 23:52:33 +0500

    Add model service

---
+++ src/services/model/model.service.ts
@@ -0,0 +1,59 @@
+import {
+	DocumentDefinition,
+	FilterQuery,
+	QueryOptions,
+	UpdateQuery,
+} from 'mongoose';
+
+import { ModelModel, ModelSansMeta, Model } from '~/models/model';
+
+export const createModel = async (
+	input: DocumentDefinition<ModelSansMeta>
+): Promise<Model> => {
+	const model = (await ModelModel.create(input)).toJSON();
+	return model;
+};
+
+export const findModels = async (
+	query: FilterQuery<Model>,
+	options?: QueryOptions
+): Promise<null | Model[]> => {
+	const models = await ModelModel.find(
+		query,
+		{},
+		options
+	).lean();
+	return models;
+};
+
+export const findModel = async (
+	query: FilterQuery<Model>,
+	options?: QueryOptions
+): Promise<null | Model> => {
+	const model = await ModelModel.findOne(
+		query,
+		{},
+		options
+	).lean();
+	return model;
+};
+
+export const findAndUpdateModel = async (
+	query: FilterQuery<Model>,
+	update: UpdateQuery<ModelSansMeta>,
+	options?: QueryOptions
+): Promise<null | Model> => {
+	const updatedModel = await ModelModel.findOneAndUpdate(
+		query,
+		update,
+		options
+	).lean();
+	return updatedModel;
+};
+
+export const deleteModel = async (
+	query: FilterQuery<Model>
+): Promise<null | Model> => {
+	const deletedModel = await ModelModel.findOneAndRemove(query).lean();
+	return deletedModel;
+};
