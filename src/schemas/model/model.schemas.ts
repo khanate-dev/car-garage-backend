6e00feb82c90e93309ed26754a22ff4f17dafa60
mbm_1607 <muhammadkhan1607@gmail.com>
2022-09-24 21:00:23 +0500

    Add model schema

---
+++ src/schemas/model/model.schemas.ts
@@ -0,0 +1,64 @@
+import { isValidObjectId, Types } from 'mongoose';
+import z from 'zod';
+
+import { createRouteSchema } from '~/helpers/schema';
+
+import { modelModelSchema } from '~/models';
+
+const body = z.strictObject({
+	name: z.string({
+		required_error: 'name is required',
+	}),
+	year: z.number({
+		required_error: 'year is required',
+	}).positive(),
+	makeTypeId: z.instanceof(Types.ObjectId).optional(),
+});
+
+const params = z.strictObject({
+	_id: z.string().refine(
+		isValidObjectId,
+		'parameter must be a valid mongo ObjectID'
+	),
+	makeTypeId: z.string().refine(
+		isValidObjectId,
+		'parameter must be a valid mongo ObjectID'
+	),
+});
+
+const response = modelModelSchema;
+
+export const createModelSchema = createRouteSchema({
+	body,
+	response,
+});
+
+export type CreateModelSchema = z.infer<typeof createModelSchema>;
+
+export const updateModelSchema = createRouteSchema({
+	body,
+	params,
+	response,
+});
+
+export type UpdateModelSchema = z.infer<typeof updateModelSchema>;
+
+export const getModelsSchema = createRouteSchema({
+	response: z.array(response),
+});
+
+export type GetModelsSchema = z.infer<typeof getModelsSchema>;
+
+export const getModelSchema = createRouteSchema({
+	params,
+	response,
+});
+
+export type GetModelSchema = z.infer<typeof getModelSchema>;
+
+export const deleteModelSchema = createRouteSchema({
+	params,
+	response,
+});
+
+export type DeleteModelSchema = z.infer<typeof deleteModelSchema>;
