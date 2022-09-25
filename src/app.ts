65a0a2fdf9fef4b4ea1afaa363772dfc56a273fa
mbm_1607 <muhammadkhan1607@gmail.com>
2022-09-25 21:32:47 +0500

    Add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import pinoMiddleWare from 'express-pino-logger';
 import helmet from 'helmet';
+import cors from 'cors';
 
 import { config } from '~/config';
 
@@ -12,6 +13,7 @@ import registerRoutes from '~/register-routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
